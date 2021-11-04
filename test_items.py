13c7f5106a5a7238de59ef6420a0f2768cfc8a01
KostopraVKo <kostopravkoko@gmail.com>
2021-11-04 17:54:31 +0300

    Update test interface language

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser):
+def test_interface_language(browser):
     browser.get(link)
-    browser.find_element_by_xpath('//button[@type="submit"]')
+    assert browser.find_element_by_xpath('//button[@type="submit"]')
     time.sleep(30)
