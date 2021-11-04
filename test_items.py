5a76c13b612c5de39b3176cd7258ad02c0423c5a
KostopraVKo <kostopravkoko@gmail.com>
2021-11-04 17:47:56 +0300

    launching autotests for different interface languages

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element_by_xpath('//button[@type="submit"]')
+    time.sleep(30)
